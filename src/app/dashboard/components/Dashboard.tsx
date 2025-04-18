ef4ab0d6d54cef0e8deb887b6e8cac36705eee43
qeryz <czkmaz@gmail.com>
2025-04-17 19:03:26 -0700

    chore: conform to new Task type

--- src/app/dashboard/components/Dashboard.tsx
+++ src/app/dashboard/components/Dashboard.tsx
@@ -11,10 +11,14 @@ interface DashboardProps {
 
 const Dashboard: React.FC<DashboardProps> = ({ tasks, addTaskMutation }) => {
   // Categorize tasks by their status
-  const toDoTasks = tasks.filter((task) => task.status === "To Do");
-  const inProgressTasks = tasks.filter((task) => task.status === "In Progress");
-  const inReviewTasks = tasks.filter((task) => task.status === "In Review");
-  const doneTasks = tasks.filter((task) => task.status === "Done");
+  const toDoTasks = tasks.filter((task) => task.status.title === "To Do");
+  const inProgressTasks = tasks.filter(
+    (task) => task.status.title === "In Progress",
+  );
+  const inReviewTasks = tasks.filter(
+    (task) => task.status.title === "In Review",
+  );
+  const doneTasks = tasks.filter((task) => task.status.title === "Done");
 
   return (
     <div className="p-8">
@@ -33,13 +37,13 @@ const Dashboard: React.FC<DashboardProps> = ({ tasks, addTaskMutation }) => {
           addTaskMutation.mutate({
             title: "New Task",
             description: "Description",
-            status: "To Do",
-            assignee: " ",
-            priority: "Low",
-            epic: "Epic 1",
-            sprint: "Sprint 1",
-            tags: ["new", "task"],
-            created_by: "User1",
+            status: 1,
+            assignee: 1,
+            priority: 1,
+            epic: 1,
+            sprint: 1,
+            tags: [1],
+            created_by: 1,
           })
         }
         className="mt-6 bg-blue-500 text-white py-2 px-4 rounded"
