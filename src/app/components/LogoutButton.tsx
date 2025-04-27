a3a833ac48ba1a8b2ed2ec85b8ee4cdff2c05b12
qeryz <czkmaz@gmail.com>
2025-04-27 16:25:32 -0700

    fix: modify logout for new auth tokens

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -1,13 +1,22 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import axiosInstance from "@/lib/api/axiosInstance";
 
 export const LogoutButton = () => {
   const router = useRouter();
 
-  const handleLogout = () => {
-    document.cookie = "token=; path=/; max-age=0; SameSite=Strict;";
-    router.push("/login");
+  const handleLogout = async () => {
+    try {
+      // Clear cookies for access and refresh tokens
+      document.cookie = "access=; path=/; max-age=0; SameSite=Strict;";
+      document.cookie = "refresh=; path=/; max-age=0; SameSite=Strict;";
+
+      // Redirect to the login page
+      router.push("/login");
+    } catch (error) {
+      console.error("Failed to log out:", error);
+    }
   };
 
   return (
