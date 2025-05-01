82c37a807b85527946ad9925439c89c4a058c61a
qeryz <czkmaz@gmail.com>
2025-05-01 11:46:03 -0700

    feat: add menu to nav bar

--- src/app/components/LogoutButton.tsx
+++ src/app/components/LogoutButton.tsx
@@ -1,11 +1,16 @@
 "use client";
 
 import { useAuth } from "@/app/context/AuthProvider";
+import { ArrowRightStartOnRectangleIcon } from "@heroicons/react/24/outline";
 export const LogoutButton = () => {
   const { logout } = useAuth();
 
   return (
-    <button onClick={logout} className="text-sm text-red-500 hover:underline">
+    <button
+      onClick={logout}
+      className="hover:bg-gray-100 w-full flex items-center justify-left text-gray-700 hover:cursor-pointer"
+    >
+      <ArrowRightStartOnRectangleIcon className="h-5 w-5 mr-3 text-black" />
       Logout
     </button>
   );
--- src/app/components/NavBar.tsx
+++ src/app/components/NavBar.tsx
@@ -1,21 +1,34 @@
 "use client";
 
+import { useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { DefaultAvatar, LogoutButton } from "./index";
+import useClickOutside from "@/hooks/useClickOutside";
 
 export const Navbar = () => {
   const pathname = usePathname();
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [isOpen, setIsOpen] = useState(false);
+
+  useClickOutside(dropdownRef, () => setIsOpen(false));
 
   if (pathname === "/login") return null;
 
   return (
     <nav className="w-full px-4 py-2 bg-gray-50 flex items-center justify-between z-50 sticky top-0 border-b border-gray-300">
       <div className="text-xl font-bold">DevBoard</div>
-      <div className="flex items-center gap-4">
-        <span className="relative group">
+      <div className="flex items-center gap-4" ref={dropdownRef}>
+        <button
+          className="flex items-center relative group"
+          onClick={() => setIsOpen((prev) => !prev)}
+        >
           <DefaultAvatar width={20} height={20} />
-        </span>
-        <LogoutButton />
+        </button>
+        {isOpen && (
+          <div className="absolute right-0 top-10 bg-white shadow-lg rounded-lg p-4 w-48 z-50">
+            <LogoutButton />
+          </div>
+        )}
       </div>
     </nav>
   );
