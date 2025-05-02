0fecdb071fa52e022fe2c1771fe6881eec3ce1c7
qeryz <czkmaz@gmail.com>
2025-05-01 19:38:47 -0700

    style: adjust styling

--- src/app/components/CustomModal.tsx
+++ src/app/components/CustomModal.tsx
@@ -38,7 +38,7 @@ export const CustomModal = ({
       >
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           {/* Modal panel */}
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
             <div className="flex items-center justify-end">
               {showCloseButton && (
                 <button
--- src/app/dashboard/components/TaskCard/components/DetailedTaskCard.tsx
+++ src/app/dashboard/components/TaskCard/components/DetailedTaskCard.tsx
@@ -17,19 +17,22 @@ const DetailedTaskCard = ({
     <CustomModal
       title={<EditableTitle task={task} />}
       body={
-        <div className="flex flex-row gap-4 items-center">
+        <div className="flex flex-col sm:flex-row gap-4 items-center w-full h-full">
           <div className="flex flex-col gap-4 w-full">
             <div className="flex items-center gap-2">
               <AssigneeSection task={task} /> {task.assignee.first_name}{" "}
               {task.assignee.last_name}
             </div>
-            <div>Status: {task.status.title}</div>
+            <div className="flex w-full">Status: {task.status.title}</div>
             <div className="flex items-center gap-2">
-              Priority: <PriorityList task={task} />
+              <span className="flex-shrink-0">Priority:</span>
+              <div className="flex-grow">
+                <PriorityList task={task} />
+              </div>
             </div>
           </div>
-          <div className="flex w-full items-center justify-center gap-2">
-            <h3>Testing</h3>
+          <div className="flex w-full items-center justify-center border-t-1 sm:border-l-1 sm:border-t-0 border-gray-200 sm:pl-4">
+            <div className="mt-4 sm:mt-0 pl-0">Testing</div>
           </div>
         </div>
       }
