bffba66a202141e60df1781eba1ccf9a02d171ff
qeryz <czkmaz@gmail.com>
2025-05-02 13:56:03 -0700

    chore: remove fragment

--- src/app/dashboard/components/TaskCard/components/CommentsSection.tsx
+++ src/app/dashboard/components/TaskCard/components/CommentsSection.tsx
@@ -10,25 +10,18 @@ export const CommentsSection = ({ taskId }: { taskId: number }) => {
       {comments.length > 0 ? (
         <ul className="flex flex-col items-start space-y-2 w-full">
           {comments.map((comment) => (
-            <>
-              <li
-                key={comment.id}
-                className="p-4 bg-gray-100 rounded-md w-full"
-              >
-                <div className="flex items-center gap-2 mb-2">
-                  <DefaultAvatar height={15} width={15} />
-                  <span className="text-sm font-semibold">
-                    {comment.user.first_name} {comment.user.last_name}
-                  </span>
-                </div>
-                <p className="text-sm text-gray-700 w-full">
-                  {comment.content}
-                </p>
-                <span className="flex text-xs text-gray-500 w-full justify-end mt-2">
-                  {formatDateAndTime(comment.created_at)}
+            <li key={comment.id} className="p-4 bg-gray-100 rounded-md w-full">
+              <div className="flex items-center gap-2 mb-2">
+                <DefaultAvatar height={15} width={15} />
+                <span className="text-sm font-semibold">
+                  {comment.user.first_name} {comment.user.last_name}
                 </span>
-              </li>
-            </>
+              </div>
+              <p className="text-sm text-gray-700 w-full">{comment.content}</p>
+              <span className="flex text-xs text-gray-500 w-full justify-end mt-2">
+                {formatDateAndTime(comment.created_at)}
+              </span>
+            </li>
           ))}
         </ul>
       ) : (
