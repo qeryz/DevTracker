e627909e2cc548a4ffc4d80de0dbb5842a2a15bf
qeryz <czkmaz@gmail.com>
2025-05-02 12:09:55 -0700

    style: remove footer and fix overflow bug

--- src/app/components/CustomModal.tsx
+++ src/app/components/CustomModal.tsx
@@ -13,7 +13,6 @@ export const CustomModal = ({
   body,
   onClose,
   showCloseButton = true,
-  footer,
 }: CustomModalProps) => {
   return (
     <div
@@ -38,7 +37,7 @@ export const CustomModal = ({
       >
         <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
           {/* Modal panel */}
-          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl">
+          <div className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-2xl min-h-[70vh] max-h-[70vh] w-full flex flex-col">
             <div className="flex items-center justify-end">
               {showCloseButton && (
                 <button
@@ -52,24 +51,23 @@ export const CustomModal = ({
             </div>
             <div
               onClick={(e) => e.stopPropagation()} // Prevent closing when clicking inside the modal
-              className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4"
+              className="flex flex-col bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4 overflow-y-auto h-full flex-grow"
             >
-              <div className="sm:flex sm:items-start w-full">
-                <div className="mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full">
+              <div className="flex flex-col sm:items-start w-full h-full flex-grow">
+                <div className="flex flex-col mt-3 text-center sm:mt-0 sm:ml-4 sm:text-left w-full h-full flex-grow">
                   <div
                     className="text-base font-semibold text-gray-900"
                     id="modal-title"
                   >
                     {title}
                   </div>
-                  <div className="mt-2 w-full">
-                    <div className="text-sm text-gray-500 w-full">{body}</div>
+                  <div className="flex flex-col mt-2 w-full h-full flex-grow">
+                    <div className="flex flex-col flex-grow text-sm text-gray-500 w-full h-full">
+                      {body}
+                    </div>
                   </div>
                 </div>
               </div>
-            </div>
-            <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-              {footer && footer}
             </div>
           </div>
         </div>
