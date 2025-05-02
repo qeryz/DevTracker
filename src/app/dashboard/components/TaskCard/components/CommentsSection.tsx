57223da62d0c2fd73fd70100e0d27fc3691406ea
qeryz <czkmaz@gmail.com>
2025-05-02 12:10:20 -0700

    chore: improve comments section formatting

--- src/app/dashboard/components/TaskCard/components/CommentsSection.tsx
+++ src/app/dashboard/components/TaskCard/components/CommentsSection.tsx
@@ -1,20 +1,34 @@
 import { useTaskComments } from "@/hooks/useTaskComments";
-
+import { formatDateAndTime } from "./utils";
+import { DefaultAvatar } from "@/app/components";
 export const CommentsSection = ({ taskId }: { taskId: number }) => {
   const comments = useTaskComments(taskId);
 
   return (
-    <div className="mt-4">
+    <div className="flex flex-col items-center flex-grow w-full gap-4">
       <h3 className="text-lg font-semibold mb-2">Comments</h3>
       {comments.length > 0 ? (
-        <ul className="space-y-2">
+        <ul className="flex flex-col items-start space-y-2 w-full">
           {comments.map((comment) => (
-            <li key={comment.id} className="p-4 bg-gray-100 rounded-md">
-              <p className="text-sm text-gray-700">{comment.content}</p>
-              <span className="text-xs text-gray-500">
-                {new Date(comment.created_at).toLocaleString()}
-              </span>
-            </li>
+            <>
+              <li
+                key={comment.id}
+                className="p-4 bg-gray-100 rounded-md w-full"
+              >
+                <div className="flex items-center gap-2 mb-2">
+                  <DefaultAvatar height={15} width={15} />
+                  <span className="text-sm font-semibold">
+                    {comment.user.first_name} {comment.user.last_name}
+                  </span>
+                </div>
+                <p className="text-sm text-gray-700 w-full">
+                  {comment.content}
+                </p>
+                <span className="flex text-xs text-gray-500 w-full justify-end mt-2">
+                  {formatDateAndTime(comment.created_at)}
+                </span>
+              </li>
+            </>
           ))}
         </ul>
       ) : (
--- src/app/dashboard/components/TaskCard/components/DetailedTaskCard.tsx
+++ src/app/dashboard/components/TaskCard/components/DetailedTaskCard.tsx
@@ -18,22 +18,37 @@ const DetailedTaskCard = ({
     <CustomModal
       title={<EditableTitle task={task} />}
       body={
-        <div className="flex flex-col sm:flex-row gap-4 items-center w-full h-full">
-          <div className="flex flex-col gap-4 w-full">
+        <div className="flex flex-col sm:flex-row h-full flex-grow text-gray-600">
+          <div className="flex flex-col gap-4 w-full flex-grow pr-4">
             <div className="flex items-center gap-2">
               <AssigneeSection task={task} /> {task.assignee.first_name}{" "}
               {task.assignee.last_name}
             </div>
-            <div className="flex w-full">Status: {task.status.title}</div>
+            <div className="flex w-full font-medium">
+              Status:{" "}
+              <span className="ml-2 font-normal">{task.status.title}</span>
+            </div>
             <div className="flex items-center gap-2">
-              <span className="flex-shrink-0">Priority:</span>
+              <span className="flex-shrink-0 font-medium">Priority:</span>
               <div className="flex-grow">
                 <PriorityList task={task} />
               </div>
             </div>
+            <div className="flex items-center gap-2">
+              <span className="flex-shrink-0 font-medium">Sprint:</span>
+              <div className="flex-grow">
+                {task.sprint ? task.sprint.title : "No sprint assigned"}
+              </div>
+            </div>
+            <div className="flex items-center gap-2">
+              <span className="flex-shrink-0">Description:</span>
+              <div className="flex-grow">
+                {task.description ? task.description : "No details provided"}
+              </div>
+            </div>
           </div>
-          <div className="flex w-full items-center justify-center border-t-1 sm:border-l-1 sm:border-t-0 border-gray-200 sm:pl-4">
-            <div className="mt-4 sm:mt-0 pl-0">
+          <div className="flex w-full items-start justify-center border-t-1 sm:border-l-1 sm:border-t-0 border-gray-200 sm:pl-4">
+            <div className="flex w-full mt-4 sm:mt-0 pl-0">
               <CommentsSection taskId={task.id} />
             </div>
           </div>
