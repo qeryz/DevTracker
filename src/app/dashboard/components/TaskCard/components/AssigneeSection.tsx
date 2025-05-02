fb04c207bd7b2c0b9e3a4bc187cd0c5c4bf2e558
qeryz <czkmaz@gmail.com>
2025-05-01 18:35:09 -0700

    fix: bug with select menu not closing

--- src/app/dashboard/components/TaskCard/components/AssigneeSection.tsx
+++ src/app/dashboard/components/TaskCard/components/AssigneeSection.tsx
@@ -1,8 +1,7 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState, useEffect } from "react";
 import { Task } from "@/lib/types/api/tasks";
 import { DefaultAvatar } from "@/app/components";
 import UserList from "./UserList";
-import useClickOutside from "@/hooks/useClickOutside";
 
 interface AssigneeSectionProps {
   task: Task;
@@ -11,11 +10,28 @@ interface AssigneeSectionProps {
 const AssigneeSection = ({ task }: AssigneeSectionProps) => {
   const [isFocused, setIsFocused] = useState(false);
   const dropdownRef = useRef<HTMLDivElement>(null);
+  const buttonRef = useRef<HTMLButtonElement>(null);
 
-  useClickOutside(dropdownRef, () => setIsFocused(false));
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        dropdownRef.current &&
+        !dropdownRef.current.contains(event.target as Node) &&
+        buttonRef.current &&
+        !buttonRef.current.contains(event.target as Node)
+      ) {
+        setIsFocused(false);
+      }
+    };
+
+    document.addEventListener("mousedown", handleClickOutside);
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, []);
 
   return (
-    <span className="relative group" ref={dropdownRef}>
+    <span className="relative group" ref={buttonRef}>
       <button onClick={() => setIsFocused((prev) => !prev)}>
         <DefaultAvatar height={20} width={20} />
       </button>
@@ -26,13 +42,14 @@ const AssigneeSection = ({ task }: AssigneeSectionProps) => {
       >
         {task.assignee.first_name} {task.assignee.last_name}
       </div>
-      <div
-        className={`absolute bottom-full text-left left-1/2 -translate-x-22 translate-y-19 rounded-lg mb-4 z-10 ${
-          isFocused ? "block" : "hidden"
-        }`}
-      >
-        <UserList task={task} />
-      </div>
+      {isFocused && (
+        <div
+          ref={dropdownRef}
+          className="absolute bottom-full text-left left-1/2 -translate-x-10 translate-y-19 rounded-lg mb-4 z-10"
+        >
+          <UserList task={task} />
+        </div>
+      )}
     </span>
   );
 };
