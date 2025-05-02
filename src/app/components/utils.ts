acf6b3ffef9812841a3c4fddb1d35b1b0a5cbad2
qeryz <czkmaz@gmail.com>
2025-05-02 14:43:48 -0700

    chore: move format date function util to app/components utils

--- src/app/components/utils.ts
+++ src/app/components/utils.ts
@@ -57,3 +57,14 @@ export const getFilterOptions = (
       return [];
   }
 };
+
+export const formatDateAndTime = (date: string) => {
+  const options: Intl.DateTimeFormatOptions = {
+    year: "numeric",
+    month: "2-digit",
+    day: "2-digit",
+    hour: "2-digit",
+    minute: "2-digit",
+  };
+  return new Date(date).toLocaleString("en-US", options);
+};
