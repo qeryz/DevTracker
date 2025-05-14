c219d08271fa69d5f404e61bae7e4890539ea1c5
qeryz <czkmaz@gmail.com>
2025-05-14 14:13:44 -0700

    chore: rename app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function HomePage() {
   return (
     <div className="text-center p-8">
-      <h1 className="text-4xl font-bold">Welcome to DevMeet!</h1>
+      <h1 className="text-4xl font-bold">Welcome to DevTracker!</h1>
     </div>
   );
 }
