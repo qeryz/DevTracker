2fe20963ab785046606ca48e042d912723afc3f0
qeryz <czkmaz@gmail.com>
2025-05-14 01:39:11 -0700

    feat: add status change via option menu

--- src/app/dashboard/components/TaskCard/components/OptionsMenu.tsx
+++ src/app/dashboard/components/TaskCard/components/OptionsMenu.tsx
@@ -1,17 +1,25 @@
 import { useRef, useState } from "react";
 import useClickOutside from "@/hooks/useClickOutside";
 import { useMutation, useQueryClient } from "react-query";
-import { deleteTask } from "@/lib/api/tasks";
+import { deleteTask, updateTask } from "@/lib/api/tasks";
+import { mapTaskToPayload } from "./utils";
+import { Task, TaskCreatePayload } from "@/lib/types/api/tasks";
 import { EllipsisVerticalIcon } from "@heroicons/react/24/outline";
+import useMiscStore from "@/store/useMiscStore"; // Import the store
+import CustomSelect from "@/app/components/CustomSelect"; // Import the CustomSelect component
 
 export interface OptionsMenuProps {
-  taskId: number;
+  task: Task;
 }
-export const OptionsMenu = ({ taskId }: OptionsMenuProps) => {
+
+export const OptionsMenu = ({ task }: OptionsMenuProps) => {
   const queryClient = useQueryClient();
   const [showOptions, setShowOptions] = useState(false);
   const [isDeleting, setIsDeleting] = useState(false);
-  const buttonRef = useRef<HTMLButtonElement>(null);
+  const divRef = useRef<HTMLDivElement>(null);
+
+  const { statuses } = useMiscStore();
+
   const deleteTaskMutation = useMutation(deleteTask, {
     onSuccess: () => {
       queryClient.invalidateQueries("tasks");
@@ -21,12 +29,34 @@ export const OptionsMenu = ({ taskId }: OptionsMenuProps) => {
       setIsDeleting(false);
     },
   });
-  const handleDelete = (e: React.MouseEvent<HTMLButtonElement>) => {
-    e.stopPropagation();
+
+  const updateTaskMutation = useMutation(
+    ({ id, updates }: { id: number; updates: TaskCreatePayload }) =>
+      updateTask(id, updates),
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries("tasks");
+      },
+    },
+  );
+
+  const handleDelete = () => {
     setIsDeleting(true);
-    deleteTaskMutation.mutate(taskId);
+    deleteTaskMutation.mutate(task.id);
   };
-  useClickOutside(buttonRef, () => setShowOptions(false));
+
+  const handleStatusChange = (newStatusId: number) => {
+    if (newStatusId !== task.status.id) {
+      const updatedTask = mapTaskToPayload(task, { status: newStatusId });
+      updateTaskMutation.mutate({
+        id: task.id,
+        updates: updatedTask,
+      });
+    }
+  };
+
+  useClickOutside(divRef, () => setShowOptions(false));
+
   return (
     <div className="relative">
       <EllipsisVerticalIcon
@@ -38,15 +68,29 @@ export const OptionsMenu = ({ taskId }: OptionsMenuProps) => {
         }}
       />
       {showOptions && (
-        <div className="absolute right-0 mt-2 w-32 bg-white border border-gray-300 rounded-md shadow-lg z-10">
+        <div
+          ref={divRef}
+          className="absolute right-0 mt-2 w-48 bg-white border border-gray-300 rounded-md shadow-lg z-10"
+        >
           <button
-            ref={buttonRef}
             aria-label="Delete Task"
             className="block cursor-pointer w-full px-4 py-2 text-sm text-left text-gray-700 hover:bg-gray-100 hover:rounded-md"
             onClick={handleDelete}
           >
             {isDeleting ? "Deleting..." : "Delete"}
           </button>
+          <div className="px-4 py-2">
+            <CustomSelect
+              label="Status"
+              options={statuses.map((status) => ({
+                id: status.id,
+                label: status.title,
+              }))}
+              selectedId={task.status.id}
+              onChange={handleStatusChange}
+              aria-label="Change Task Status"
+            />
+          </div>
         </div>
       )}
     </div>
